2f01ed33035a758c21446425da828047be624753
David Sotomayor <david.elijah.sotomayor@gmail.com>
2020-12-16 23:38:14 -0600

    create ajax api calls and logged object returns

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
 $( document ).ready(function() {
 var apiKey = 'f8c674028f7d3e9fc14527ccfada55ec'
-var weatherAPI = 'http://api.openweathermap.org/data/2.5/forecast?q=Chicago&units=imperial&appid=' + apiKey;
+var forecastWeatherAPI = 'http://api.openweathermap.org/data/2.5/forecast?q=Chicago&units=imperial&appid=' + apiKey;
+var currentWeatherAPI = 'http://api.openweathermap.org/data/2.5/weather?q=Chicago&units=imperial&appid=' + apiKey;
+var uvIndex = 'http://api.openweathermap.org/data/2.5/uvi?lat=41.85&lon=-87.65&appid=' + apiKey;
 
+// returns object containing 5 day forecast
 $.ajax({
-    url: weatherAPI,
+    url: forecastWeatherAPI,
     method: "GET"
     })
     .then(function(response) {
-        console.log(weatherAPI)
+        console.log(forecastWeatherAPI)
+        console.log(response)
+    })
+
+// returns object containing current day forecast
+$.ajax({
+    url: currentWeatherAPI,
+    method: "GET"
+    })
+    .then(function(response) {
+        console.log(currentWeatherAPI)
+        console.log(response)
+    })
+
+// returns object containing current day UV index
+$.ajax({
+    url: uvIndex,
+    method: "GET"
+    })
+    .then(function(response) {
+        console.log(uvIndex)
         console.log(response)
     })
 })
